00c1f0a1e23ccea8f3d59b15231b62c65ca9d939
Sean Dolbec <sean@example.com>
2026-09-23 12:04:54 -0400

    Pin Swartzit to 0.1.15 release

--- Formula/swartzit.rb
+++ Formula/swartzit.rb
@@ -1,9 +1,9 @@
 class Swartzit < Formula
   desc "Self-hosted, pseudonymous discussion community"
   homepage "https://stoverparc.org"
-  url "https://github.com/techmore/swartzit/archive/refs/tags/v0.1.14-20260923T15.tar.gz"
-  version "0.1.14.20260923.15"
-  sha256 "c393f1eaee1a49a6548600b4de6887d11f324d79198c43ed407000b65b6c8bed"
+  url "https://github.com/techmore/swartzit/archive/refs/tags/v0.1.15-20260923T16.tar.gz"
+  version "0.1.15.20260923.16"
+  sha256 "b6e1801eabed60dfff1d0bad9acd67968e56ccccd504211c41195d465c63d6da"
 
   depends_on "node" => :build
   depends_on "rust" => :build
